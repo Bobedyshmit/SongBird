f4b730342010c6bf3bdef579e4fb7319e42af4be
Bobedyshmit <Bobedyshmit@users.noreply.github.com>
2015-12-20 22:33:46 -0500

    I'm feeling trapped in all this commitment

---
+++ src/main/java/songbird/core/config/AppConfig.java
@@ -0,0 +1,10 @@
+package songbird.core.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("songbird.core")
+public class AppConfig {
+
+}
